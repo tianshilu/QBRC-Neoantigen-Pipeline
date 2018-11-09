74e67a024991186592ed5be95dad81f6304d613c
Neoantigen-pipeline <41589336+Neoantigen-pipeline@users.noreply.github.com>
2018-11-08 19:43:17 -0600

    Add files via upload

---
+++ script/expression.pl
@@ -0,0 +1,84 @@
+# prerequisite in path: featureCounts, STAR, mixcr
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Cwd 'abs_path';
+
+# bam file
+# gtf file
+# output folder
+# number of threads to use
+my ($bam_file,$gtf,$output_folder,$thread)=@ARGV;
+my ($path,$index,$fastq);
+my $parameters=" --primary -O -t exon -g transcript_id -s 0 -T ".$thread." --largestOverlap --minOverlap 3 --ignoreDup -p -P -B -C";
+my $compress=" --readFilesCommand zcat";
+
+#################  clean data  ###############################
+
+$path=abs_path($0);
+$path=~s/expression\.pl//;
+
+$bam_file=~/^(.*)\:(.*)$/;
+$index=$1;
+$fastq=$2;
+
+# if giving bam files
+if ($fastq=~s/^bam,//)
+{ 
+  system("perl ".$path."/bam2fastq.pl ".$fastq." ".$output_folder." ".$thread);
+  $fastq=$output_folder."/fastq1.fastq ".$output_folder."/fastq2.fastq";
+  $compress="";
+}else # if giving fastq file(s)
+{
+  $fastq=~s/,/ /;
+}
+
+#################  expression analyses  ######################
+
+# STAR alignment
+system("STAR --runThreadN ".$thread." --genomeDir ".$index." --readFilesIn ".$fastq." --outFileNamePrefix ".$output_folder.
+  "/ --outSAMtype BAM Unsorted".$compress);
+$bam_file=$output_folder."/Aligned.out.bam";
+
+# featureCounts
+unless (-f $gtf) {die "Error: Gtf annotation file doesn't exists!\n";}
+unless (-f $bam_file) {die "Error: RNA-Seq bam file doesn't exists!\n";}
+
+system("cd ".$output_folder." && sambamba view -h -t ".$thread." ".$bam_file." > aligned.sam");
+system("cd ".$output_folder." && featureCounts -a ".$gtf." -o ".$output_folder."/transcript.featureCounts aligned.sam ".$parameters);
+system("cd ".$output_folder." && featureCounts -a ".$gtf." -o ".$output_folder."/exon.featureCounts aligned.sam -f ".$parameters);
+system("rm -f ".$output_folder."/aligned.sam");
+
+unless (-f $output_folder."/transcript.featureCounts") {die "Error: featureCounts failed!\n";}
+unless (-f $output_folder."/exon.featureCounts") {die "Error: featureCounts failed!\n";}
+
+# cleanup
+unlink($output_folder."/transcript.featureCounts.summary");
+unlink($output_folder."/exon.featureCounts.summary");
+unlink($output_folder."/Log.out");
+unlink($output_folder."/Log.progress.out");
+unlink($output_folder."/SJ.out.tab");
+unlink($output_folder."/Log.final.out");
+unlink($bam_file);
+
+###################  T cell repertoire analyses  #################
+
+system("mixcr align -s hs -f -p rna-seq -t ".$thread." -OallowPartialAlignments=true ".$fastq." ".
+  $output_folder."/alignments.vdjca > ".$output_folder."/mixcr1.txt");
+system("mixcr assemblePartial -f ".$output_folder."/alignments.vdjca ".$output_folder.
+  "/alignment_contigs.vdjca > ".$output_folder."/mixcr2.txt");
+system("mixcr assemble -f -t ".$thread." ".$output_folder."/alignment_contigs.vdjca ".$output_folder.
+  "/clones.clns > ".$output_folder."/mixcr3.txt");
+system("mixcr exportClones -f ".$output_folder."/clones.clns ".$output_folder.
+  "/clones.txt > ".$output_folder."/mixcr4.txt");
+
+unlink($output_folder."/fastq1.fastq");
+unlink($output_folder."/fastq2.fastq");
+unlink($output_folder."/alignments.vdjca");
+unlink($output_folder."/alignment_contigs.vdjca");
+unlink($output_folder."/clones.clns");
+unlink($output_folder."/mixcr1.txt");
+unlink($output_folder."/mixcr2.txt");
+unlink($output_folder."/mixcr3.txt");
+unlink($output_folder."/mixcr4.txt");
+
---
+++ script/filter_somatic.R
@@ -0,0 +1,76 @@
+#############  read data  ######################
+
+args <- commandArgs(trailingOnly = TRUE)
+input=args[1]
+tumor_freq=args[2]
+normal_freq=args[3]
+output=args[4]
+rpkm_cutoff=as.numeric(args[5])
+code_path=args[6]
+max_mutations=as.numeric(args[7])
+
+unlink(output)
+if (!file.exists(input)) {stop(paste(input,"doesn't exist!\n"))}
+mutations=read.table(input,sep="\t",stringsAsFactors = F,header=T)
+
+#########  filter SNPs  ######################
+
+# unique mutations in each patient
+mutations$Variant=apply(mutations[,c("Chr","Start","Ref","Alt")],1,function(x) paste(x,collapse="_"))
+mutations=mutations[!duplicated(mutations$Variant),]
+
+# non-silent mutations
+mutations=mutations[mutations$Func.refGene %in% c("exonic","exonic;splicing",
+                                                  "splicing;exonic"),]
+mutations=mutations[mutations$ExonicFunc.refGene %in% c("nonsynonymous SNV",
+  "frameshift substitution","nonframeshift substitution","stoploss"),]
+
+# filter SNPs by allele frequencies
+mutations=mutations[mutations$Normal_alt/(mutations$Normal_alt+mutations$Normal_ref)<
+  as.numeric(normal_freq),]
+mutations=mutations[mutations$Tumor_alt/(mutations$Tumor_alt+mutations$Tumor_ref)>
+  as.numeric(tumor_freq),]
+if (dim(mutations)[1]==0) 
+{ 
+  warning("No somatic mutations in tumor sample are left after all filtering(1)!")
+  q()
+}
+
+# if a gene has too many mutations in one patient, it is likely to be false or a real complicated varaint
+# and will cause a problem in protein translation
+tmp=table(mutations$Gene)
+mutations=mutations[!mutations$Gene %in% names(tmp[tmp>=5]),]
+
+# pre-filter by transcript expression, not accurate
+if (rpkm_cutoff>0)
+{
+  source(paste(code_path,"/expressed_neoantigen_function.R",sep=""))
+  transcript_file=sub("neoantigen$","transcript.featureCounts",input,perl=T)
+  transcript_exp=read.table(transcript_file,stringsAsFactors = F,header=T)[,c(1,6,7)]
+  colnames(transcript_exp)[3]="expression"
+  transcript_exp=rpkm(transcript_exp)
+  transcript_split=strsplit(mutations$AAChange.refGene,",")
+  mutations$keep=T
+  for (i in 1:dim(mutations)[1])
+  {
+    transcripts=
+      sapply(1:length(transcript_split[[i]]),function(j) strsplit(transcript_split[[i]][j],":")[[1]][2])
+    if (sum(transcript_exp[,1] %in% transcripts)<1 || 
+        all(transcript_exp[transcript_exp[,1] %in% transcripts,3]<rpkm_cutoff))
+      {mutations$keep[i]=F}
+  }
+  mutations=mutations[mutations$keep==T,]
+}
+
+if (dim(mutations)[1]==0 || dim(mutations)[1]>max_mutations) 
+{ 
+  warning(paste("No or too many somatic mutations (",dim(mutations)[1],
+    ") in tumor sample are left after all filtering!",sep=""))
+  q()
+}
+
+##########  write data  ################
+
+mutations$Chr=sub("chr","",mutations$Chr) 
+write.table(mutations[,c("Chr","Start","End","Ref","Alt","Tumor_ref","Tumor_alt","Normal_ref",
+  "Normal_alt")],file=output,sep="\t",quote=F,row.names = F,col.names =F)
